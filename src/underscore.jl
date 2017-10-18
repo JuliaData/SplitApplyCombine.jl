413c33d39f17b7961eb4b6719de2791acd193623
Chris Foster <chris42f@gmail.com>
2017-10-19 07:43:59 +1000

    Remove warnings by replacing _ with a gensym

--- src/underscore.jl
+++ src/underscore.jl
@@ -1,3 +1,16 @@
+# Really dumb variable name replacement macro.  Doesn't know about scopes or
+# the gory details of any `Expr`s
+function replace_var!(ex, varname, replacement)
+    if ex isa Expr
+        map!(e->replace_var!(e, varname, replacement), ex.args, ex.args)
+        return ex
+    elseif ex === varname
+        return replacement
+    else
+        return ex
+    end
+end
+
 function expand_underscores_!(ex)
     if ex === :_
         return true
@@ -14,15 +27,28 @@ function expand_underscores_!(ex)
     end
 end
 
-
 function expand_underscores!(ex)
     if expand_underscores_!(ex)
-        return Expr(:->, :_, ex)
+        return replace_var!(Expr(:->, :_, ex), :_, gensym("_"))
     else
         ex
     end
 end
 
+"""
+    @_ ex
+
+Expand an expression containing underscore placeholders into a lambda.  The
+lambda head is placed at the outermost scope, except where interrupted by `|>`
+pipe operators.
+
+# Examples
+
+`data |> reduce(+,_)`  expands to  `data |> x->reduce(+,_)`
+
+`data |> foo(bar(_))`  expands to  `data |> x->foo(bar(x))
+
+"""
 macro _(ex)
     esc(expand_underscores!(ex))
 end
