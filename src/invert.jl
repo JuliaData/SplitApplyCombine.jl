d7eb18b994579f3d295faa8089e2f1dde6983f35
Andy Ferris <andy.ferris@roames.com.au>
2018-06-18 22:30:53 +1000

    WIP invert on tuples/arrays

--- src/invert.jl
+++ src/invert.jl
@@ -17,7 +17,7 @@ julia> invert([[1,2,3],[4,5,6]])
  [2, 5]
  [3, 6]
 ```
-"""
+""" # Array-Array
 @inline function invert(a::AbstractArray{T}) where {T <: AbstractArray}
     f = first(a)
     innersize = size(a)
@@ -79,3 +79,71 @@ function _invert!(out, a, innerkeys, outerkeys)
     end
     return out
 end
+
+# Tuple-tuple
+if VERSION < v"0.7-"
+    @generated function invert(a::NTuple{n, NTuple{m, Any}}) where {n, m}
+        exprs = [:(tuple($([:(a[$j][$i]) for j = 1:n]...))) for i = 1:m]
+        return quote
+            Base.@_inline_meta
+            return :(tuple($(exprs...)))
+        end
+    end
+else
+    @inline function invert(a::NTuple{n, NTuple{m, Any}}) where {n, m}
+        if @generated
+            exprs = [:(tuple($([:(a[$j][$i]) for j = 1:n]...))) for i = 1:m]
+            return :(tuple($(exprs...)))
+        else
+            ntuple(i -> ntuple(j -> a[j][i], Val(n)), Val(m))
+        end
+    end
+end
+
+# Tuple-Array
+@inline function invert(a::NTuple{n, AbstractArray}) where {n}
+    arraysize = keys(a[1])
+
+    @boundscheck for x in a
+        if keys(x) != arraysize
+            error("keys don't match")
+        end
+    end
+    
+    # TODO: Construct empty array and call invert! Fix inference issues.
+    [ntuple(i -> a[i][j], Val(n)) for j = arraysize]
+end
+
+# TODO: invert!
+
+# Array-Tuple
+@inline function invert(a::AbstractArray{<:NTuple{n, Any}}) where {n}
+    arraysize = keys(a[1])
+    
+    # TODO fix inference issues.
+    ntuple(i -> [a[j][i] for j = arraysize], Val(n))
+end
+
+# NamedTuple-NamedTuple
+
+# NamedTuple-Array
+
+# Array-NamedTuple
+
+# NamedTuple-Tuple
+
+# Tuple-NamedTuple
+
+# Dict-Dict
+
+# Dict-Array
+
+# Array-Dict
+
+# Dict-Tuple
+
+# Tuple-Dict
+
+# NamedTuple-Dict
+
+# Dict-NamedTuple
--- test/invert.jl
+++ test/invert.jl
@@ -5,4 +5,14 @@
     
     b = [[0,0],[0,0],[0,0]]
     @test invert!(b, a) == [[1,4],[2,5],[3,6]]
+
+    # tuples of tuples
+    tt = ((true, 2.0, 3f0), ('d', 0x05, 6))
+    @test @inferred(invert(tt)) === ((true, 'd'), (2.0, 0x05), (3f0, 6))
+
+    # tuples / arrays
+    ta = ([1, 2], [3.1, 4.1])
+    at = [(1, 3.1), (2, 4.1)]
+    @test @inferred(invert(ta))::Vector{Tuple{Int, Float64}} == at
+    @test @inferred(invert(at))::Tuple{Vector{Int}, Vector{Float64}} == ta
 end
