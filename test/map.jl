caf2c9d92a19c07c9cdb3e14fc1e2bd5b69cc618
Alexander <alexander@plav.in>
2021-11-09 03:09:06 +0300

    add filterview() function (#41)

--- test/map.jl
+++ test/map.jl
@@ -22,3 +22,10 @@ end
     @test @inferred(first(iter2)) === 2
     @test collect(iter2) == [2, 4, 6]
 end
+
+@testset "filterview" begin
+    a = [1, 2, 3]
+    @test @inferred(filterview(x -> x >= 2, a)) == [2, 3]
+    filterview(x -> x >= 2, a)[1] = 10
+    @test a == [1, 10, 3]
+end
