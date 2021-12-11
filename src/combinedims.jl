8d815747fa50a807e397cd80ec135fbde00a9bb1
Alexander Plavin <alexander@plav.in>
2021-12-12 01:33:00 +0300

    support arbitrary dimensions in combinedims

--- src/combinedims.jl
+++ src/combinedims.jl
@@ -16,21 +16,31 @@ julia> combinedims([[1, 2], [3, 4]])
  2  4
 ```
 """
-combinedims(a::AbstractArray{<:AbstractArray{<:Any, N}, M}) where {N,M} = _combinedims(a, Val(ntuple(identity, Val(N))))
+combinedims(a::AbstractArray{<:AbstractArray{<:Any, N}, M}) where {N,M} = combinedims(a, ntuple(i -> N + i, M))
+combinedims(a::AbstractArray{<:AbstractArray{<:Any, N}, 1}, outerdim::Int) where {N} = combinedims(a, (outerdim,))
+combinedims(a::AbstractArray{<:AbstractArray{<:Any, N}, M}, outerdims::NTuple{M, Int}) where {N, M} = _combinedims(a, Val(outerdims))
 
-function _combinedims(a::AbstractArray, ::Val{dims}) where {dims}
+function _combinedims(a::AbstractArray, ::Val{outer_dims}) where {outer_dims}
     outeraxes = axes(a)
     inneraxes = _inneraxes(a)
-    newaxes = (inneraxes..., outeraxes...) # TODO support permutation
-    T = inner_eltype(a)
+    ndims_total = length(outeraxes) + length(inneraxes)
+    newaxes = _combine_tuples(ndims_total, outer_dims, outeraxes, inneraxes)
 
+    T = inner_eltype(a)
     out = similar(a, T, newaxes)
-    for i in CartesianIndices(inneraxes)
-        for j in CartesianIndices(outeraxes)
-            out[i.I..., j.I...] = a[j][i]
-        end
+    for j in CartesianIndices(outeraxes)
+        I = slice_inds(j, Val(outer_dims), Val(ndims_total))
+        out[I..., :] = a[j]  # trailing ':' is required for zero-dimensional a[j]
     end
     return out
+end
+
+@inline function _combine_tuples(n, dims, t_then, t_else)
+    i_out = Ref(0)
+    i_in = Ref(0)
+    ntuple(n) do i
+        i ∈ dims ? t_then[i_out[] += 1] : t_else[i_in[] += 1]
+    end
 end
 
 _inneraxes(a) = axes(first(a)) # Can specialize this for static arrays, for example
@@ -41,20 +51,29 @@ inner_eltype(a::AbstractArray{<:AbstractArray{T}}) where {T} = T
 
 ## Lazy version
 
-struct CombineDimsArray{T, N, A} <: AbstractArray{T, N}
+struct CombineDimsArray{T, N, Nout, A} <: AbstractArray{T, N}
     parent::A
+    outer_dims::NTuple{Nout, Int}
 end
 
 Base.parent(a::CombineDimsArray) = a.parent
 
-Base.size(a::CombineDimsArray) = (size(first(parent(a)))..., size(parent(a))...)
-axes(a::CombineDimsArray) = (axes(first(parent(a)))..., axes(parent(a))...)
+@inline dims_outer(a::CombineDimsArray) = a.outer_dims
+@inline function dims_inner(a::CombineDimsArray{T, N, Nout}) where {T, N, Nout}
+    # below is a type-stable version of:
+    # filter(∉(dims_outer(a)), ntuple(identity, N))
+    i = Ref(0)
+    ntuple(N - Nout) do _
+        i[] = findnext(∉(dims_outer(a)), 1:N, i[] + 1)
+    end
+end
+
+Base.size(a::CombineDimsArray) = _combine_tuples(ndims(a), dims_outer(a), size(parent(a)), size(first(parent(a))))
+axes(a::CombineDimsArray) = _combine_tuples(ndims(a), dims_outer(a), axes(parent(a)), axes(first(parent(a))))
 Base.IndexStyle(::CombineDimsArray) = Base.IndexCartesian()
 @propagate_inbounds function Base.getindex(a::CombineDimsArray{T, N}, i::Vararg{Int, N}) where {T, N}
-    outer_ndims = ndims(parent(a))
-    inner_ndims = N - outer_ndims
-    outer_inds = ntuple(j -> @inbounds(return i[j+inner_ndims]), Val(outer_ndims))
-    inner_inds = ntuple(j -> @inbounds(return i[j]), Val(inner_ndims))
+    outer_inds = getindices(i, dims_outer(a))
+    inner_inds = getindices(i, dims_inner(a))
     return parent(a)[outer_inds...][inner_inds...]
 end
 
@@ -67,6 +86,6 @@ This is the inverse operation of `splitdims` / `splitdimsview`.
 
 See also `combinedims`, the eager version of this function.
 """
-function combinedimsview(a::AbstractArray{<:AbstractArray{T, N}, M}) where {T, N, M}
-    return CombineDimsArray{inner_eltype(a), N + M, typeof(a)}(a)
-end
+combinedimsview(a::AbstractArray{<:AbstractArray{T, N}, M}) where {T, N, M} = combinedimsview(a, ntuple(i -> N + i, Val(M)))
+combinedimsview(a::AbstractArray{<:AbstractArray{T, N}, 1}, outerdim::Int) where {T, N} = combinedimsview(a, (outerdim,))
+combinedimsview(a::AbstractArray{<:AbstractArray{T, N}, M}, outerdims::NTuple{M, Int}) where {T, N, M} = CombineDimsArray{inner_eltype(a), N + M, M, typeof(a)}(a, outerdims)
