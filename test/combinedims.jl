72d7d6d9c525b430e835b504a08034c5620df55f
Alexander <alexander@plav.in>
2022-01-18 16:09:52 +0300

    support arbitrary dimensions in combinedims (#45)

--- test/combinedims.jl
+++ test/combinedims.jl
@@ -1,8 +1,30 @@
 @testset "combinedims" begin
-    #m = [1 2 3 4; 5 6 7 8; 9 10 11 12]
     m = [1 5 9; 2 6 10; 3 7 11; 4 8 12]
     vv = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-    @test combinedims(vv) == m
 
-    @test combinedimsview(vv) == m
+    @testset for combine in (combinedims, combinedimsview)
+        @test @inferred(size(combine(vv))) == size(m)
+        @test @inferred(axes(combine(vv))) == axes(m)
+        @test @inferred(combine(vv)) == m
+        @test @inferred(combine(vv)[2, 1]) == m[2, 1]
+    
+        @test @inferred(size(combine(vv, 2))) == size(m)
+        @test @inferred(axes(combine(vv, 2))) == axes(m)
+        @test @inferred(combine(vv, 2)) == m
+        @test @inferred(combine(vv, 2)[2, 1]) == m[2, 1]
+        @test @inferred(combine(vv, 1)) == permutedims(m)
+
+        A = rand(2, 3, 5, 7)
+        @test @inferred(combine(splitdims(A))) == A
+        @testset for d in [1:ndims(A); (1, 2); (1, 3); (2, 3); (2, 4); (1, 2, 3); (1, 2, 4); (2, 3, 4); (1, 2, 3, 4)]
+            @test @inferred(combine(splitdims(A, d), d)) == A
+            @test @inferred(combine(splitdims(A, d), d)[1, 2, 3, 4]) == A[1, 2, 3, 4]
+        end
+    end
+
+    c_copy = combinedims(vv)
+    c_view = combinedimsview(vv)
+    vv[1][3] = 100
+    @test c_copy[3, 1] == 3
+    @test c_view[3, 1] == 100
 end
